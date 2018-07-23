bedeeb6a397e9e9ee60b3a6b9ee41f18560d3458
rajat0503 <33680004+rajat0503@users.noreply.github.com>
2018-07-23 17:29:53 +0530

    tap file related changes

--- runtestci.m
+++ runtestci.m
@@ -5,16 +5,22 @@ jenkins_workspace = getenv('WORKSPACE');
 cd(jenkins_workspace);
 
 try
-    
-    suite = testsuite('testman','Name','SLTEST_TC_*'); %this the new change
-    
-    
+    mcdcMet = CoverageMetrics('Decision',true,'Condition',true,'MCDC',true);
+covSettings = ModelCoveragePlugin('RecordModelReferenceCoverage',true,...
+     'Collecting',mcdcMet);
+    suite = testsuite('testman'); %Use SLTEST testman.mldatx file to create testsuite
     % Create and configure the runner
     
+    % TAP File Creation For Test Anything Protocol Integration
     tapResultsFile = fullfile(jenkins_workspace, 'TAPResults.tap');
+    % new Tap File Creation 
+    % TAP File Created in Jenkins Workspace Path
+    
     runner = TestRunner.withTextOutput();
     runner.addPlugin(TAPPlugin.producingVersion13(ToFile(tapResultsFile)));
-		
+%     addPlugin(runner,covSettings);
+%  	coverageFile = fullfile(jenkins_workspace, 'coverage.xml');
+%     addCoberturaCoverageIfPossible(runner, jenkins_workspace, coverageFile);	
 	% Run tests
 results = runner.run(suite);
 %     results2 = runtests('testman.mldatx');
@@ -23,7 +29,5 @@ results = runner.run(suite);
 %      display(results2);
 catch e
     disp(getReport(e, 'extended'));
-    exit(1);
+ 
 end
-
-
